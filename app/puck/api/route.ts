b0156f0c8362d5d83a8859872cfa97ca988f503d
Silvio Brändle <silvio@silviobraendle.ch>
2025-01-13 09:09:26 +0100

    Refactor database (#18)
    * Refactor database
    
    * Use server on db functions and revalidate path on change

--- app/puck/api/route.ts
+++ app/puck/api/route.ts
@@ -1,22 +1,11 @@
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
-import fs from "fs";
+import { savePage } from "@lib/database";
 
 export async function POST(request: Request) {
   const payload = await request.json();
 
-  const existingData = JSON.parse(
-    fs.existsSync("database.json")
-      ? fs.readFileSync("database.json", "utf-8")
-      : "{}"
-  );
-
-  const updatedData = {
-    ...existingData,
-    [payload.path]: payload.data,
-  };
-
-  fs.writeFileSync("database.json", JSON.stringify(updatedData));
+  await savePage(payload.path, payload.data);
 
   // Purge Next.js cache
   revalidatePath(payload.path);
