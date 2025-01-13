ff3243e8f411482f08e232f31bd5e80c8129f66f
Cuerillius <149763743+Cuerillius@users.noreply.github.com>
2025-01-13 09:19:24 +0100

    VerticalSpace Component (#16)
    * add Hero component and SectionDivider
    
    * refactor conditional rendering
    
    * add VerticalSpace component
    
    * add Hero component and SectionDivider
    
    * refactor conditional rendering
    
    * add VerticalSpace component
    
    * fix naming

--- config/page.config.ts
+++ config/page.config.ts
@@ -1,18 +1,23 @@
 import type { Config } from "@measured/puck";
-import type { TextProps } from "@components/Text";
 import { textConfig } from "@components/Text";
+import type { TextProps } from "@components/Text";
 import { heroConfig } from "@components/Hero";
 import type { HeroProps } from "@components/Hero";
+import { verticalSpaceConfig } from "@components/VerticalSpace";
+import type { VerticalSpaceProps } from "@components/VerticalSpace";
+
 
 type Props = {
     Text: TextProps;
     Hero: HeroProps;
+    VerticalSpace: VerticalSpaceProps;
 };
 
 export const config: Config<Props> = {
     components: {
         Text: textConfig,
         Hero: heroConfig,
+        VerticalSpace: verticalSpaceConfig,
     },
 };
 
