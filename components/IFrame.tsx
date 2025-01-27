7478da4525933c0aac89cffd82908e507de5141a
g1ulia <145566901+gV1uLLia@users.noreply.github.com>
2025-01-27 09:58:14 +0100

    IFrame Component (#28)
    * finish IFrame component
    
    * add changes
    
    * remove unused changes
    
    * Revert "add changes"
    
    This reverts commit fd10c5799bede2efee9b88d2d6d7fdabfbcdc5ac.
    
    * update IFrame.tsx

---
+++ components/IFrame.tsx
@@ -0,0 +1,32 @@
+import { ComponentConfig } from '@measured/puck';
+import React from 'react';
+
+export type IFrameProps = {
+    source: string;
+    height: string;
+    title: string;
+}
+
+
+function IFrame({ source, height, title }: IFrameProps) {
+  return <>
+  <div className='w-full overflow-hidden'>
+    <iframe src={source} width='100%' height={height} title={title}/>
+  </div>
+  </>
+}
+
+export const iframeConfig: ComponentConfig<IFrameProps> = {
+    render: IFrame,
+    fields: {
+        source: {
+            type: "textarea",
+        },
+        height: {
+            type: "number",
+        },
+        title: {
+            type: "textarea",
+        },
+    },
+};
--- config/page.config.ts
+++ config/page.config.ts
@@ -5,6 +5,8 @@ import { heroConfig } from "@components/Hero";
 import type { HeroProps } from "@components/Hero";
 import { verticalSpaceConfig } from "@components/VerticalSpace";
 import type { VerticalSpaceProps } from "@components/VerticalSpace";
+import { iframeConfig } from "@components/IFrame";
+import type { IFrameProps } from "@components/IFrame";
 import { flexConfig } from "@components/Flex";
 import { FlexProps } from "@components/Flex";
 
@@ -13,6 +15,7 @@ type Props = {
   Hero: HeroProps;
   VerticalSpace: VerticalSpaceProps;
   Flex: FlexProps;
+  IFrame: IFrameProps;
 };
 
 export const config: Config<Props> = {
@@ -21,6 +24,7 @@ export const config: Config<Props> = {
     Hero: heroConfig,
     VerticalSpace: verticalSpaceConfig,
     Flex: flexConfig,
+    IFrame: iframeConfig,
   },
 };
 
