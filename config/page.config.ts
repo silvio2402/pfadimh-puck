671a35a96bf241b5030d0c95b402975049221f0a
Gabriel-A-NY <145563844+Gabriel-A-NY@users.noreply.github.com>
2025-01-27 08:32:03 +0100

    make flex component (#17)
    * make flex component
    
    * change namig
    
    * remove unused files and improve code consistency
    
    * make improvements regarding requested changes

--- config/page.config.ts
+++ config/page.config.ts
@@ -5,20 +5,23 @@ import { heroConfig } from "@components/Hero";
 import type { HeroProps } from "@components/Hero";
 import { verticalSpaceConfig } from "@components/VerticalSpace";
 import type { VerticalSpaceProps } from "@components/VerticalSpace";
-
+import { flexConfig } from "@components/Flex";
+import { FlexProps } from "@components/Flex";
 
 type Props = {
-    Text: TextProps;
-    Hero: HeroProps;
-    VerticalSpace: VerticalSpaceProps;
+  Text: TextProps;
+  Hero: HeroProps;
+  VerticalSpace: VerticalSpaceProps;
+  Flex: FlexProps;
 };
 
 export const config: Config<Props> = {
-    components: {
-        Text: textConfig,
-        Hero: heroConfig,
-        VerticalSpace: verticalSpaceConfig,
-    },
+  components: {
+    Text: textConfig,
+    Hero: heroConfig,
+    VerticalSpace: verticalSpaceConfig,
+    Flex: flexConfig,
+  },
 };
 
 export default config;
